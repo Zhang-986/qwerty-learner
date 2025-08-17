e9768da2519759b009bc1b36baa085bd7ef17b02
Zhang-986 <231451081632@stu.haust.edu.cn>
2025-08-17 17:18:20 +0800

    adc

--- src/pages/Typing/components/WordPanel/components/Phonetic/index.tsx
+++ src/pages/Typing/components/WordPanel/components/Phonetic/index.tsx
@@ -12,12 +12,12 @@ function Phonetic({ word }: PhoneticProps) {
 
   return (
     <div
-      className={`space-x-5 text-center text-sm font-normal text-gray-600 transition-colors duration-300 dark:text-gray-400 ${
+      className={`space-x-5 text-center text-lg font-medium text-gray-600 transition-colors duration-300 dark:text-gray-400 ${
         isTextSelectable && 'select-text'
       }`}
     >
-      {phoneticConfig.type === 'us' && word.usphone && word.usphone.length > 1 && <span>{`AmE: [${word.usphone}]`}</span>}
-      {phoneticConfig.type === 'uk' && word.ukphone && word.ukphone.length > 1 && <span>{`BrE: [${word.ukphone}]`}</span>}
+      {phoneticConfig.type === 'us' && word.usphone && word.usphone.length > 1 && <span>{` [${word.usphone}]`}</span>}
+      {phoneticConfig.type === 'uk' && word.ukphone && word.ukphone.length > 1 && <span>{` [${word.ukphone}]`}</span>}
     </div>
   )
 }
